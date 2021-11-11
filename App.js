efb1d4d92ab3b3c8d6446319c22322c1156971d2
Uwem Akan Nkereuwem <uwemakan12@gmail.com>
2021-11-11 04:27:06 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar as ExpoStatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar
+} from 'react-native';
+
+const isAndroid = Platform.OS === 'android';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Our react native blank canvas</Text>
-    </View>
+    <>
+      <SafeAreaView
+        style={{flex: 1, marginTop: isAndroid ? StatusBar.currentHeight : 0}}
+      >
+        <View style={{padding: 16, backgroundColor: 'green'}}>
+          <Text>search</Text>
+        </View>
+        <View style={{flex: 1, padding: 16, backgroundColor: 'blue'}}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
@@ -15,6 +34,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });

0ebeb28564030dc276c6c2f70df345436587aff5
Uwem Akan Nkereuwem <uwemakan12@gmail.com>
2021-11-21 22:54:15 +0100

    firebase functions

--- functions/geocode/index.js
+++ functions/geocode/index.js
@@ -1,5 +1,5 @@
 const url = require('url');
-// const functions = require('firebase-functions');
+const functions = require('firebase-functions');
 
 const { locations: locationsMock } = require('./geocode.mock');
 
@@ -9,14 +9,14 @@ exports.geocodeRequest = (req, res, client) => {
     const locationMock = locationsMock[city.toLowerCase()];
     return res.json(locationMock);
   }
-  // return client
-  //   .geocode({
-  //     params: {
-  //       address: city,
-  //       key: functions.config().google.key,
-  //     },
-  //     timeout: 10000,
-  //   })
-  //   .then((response) => res.json(response.data))
-  //   .catch((e) => res.status(400).send(e.response.data.error_message));
+  return client
+    .geocode({
+      params: {
+        address: city,
+        key: functions.config().google.key,
+      },
+      timeout: 10000,
+    })
+    .then((response) => res.json(response.data))
+    .catch((e) => res.status(400).send(e.response.data.error_message));
 };
